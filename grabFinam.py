6a4bb4ccca63c7d32b0c1dcb7958af7859d5920b
Vitaly Perov <vitperov@gmail.com>
2020-02-08 21:48:45 +0300

    Save quote to own file

--- grabFinam.py
+++ grabFinam.py
@@ -2,26 +2,25 @@
 """
 Created on Sat Jun 24 01:46:38 2017
 
-@author: optimusqp
+@author: optimusqp 2017
+Perov Vitaly 2020
 https://habr.com/ru/post/332700/
 """
 import urllib
 
-
-
 def quotes(code,year_start,month_start,day_start,year_end,month_end,day_end,e,market,em,df,mf,yf,dt,mt,yt,p,dtf,tmf,MSOR,mstimever,sep,sep2,datf,at):
     
     page = urllib.urlopen('http://export.finam.ru/'+str(code)+'_'+str(year_start)+str(month_start)+str(day_start)+'_'+str(year_end)+str(month_end)+str(day_end)+str(e)+'?market='+str(market)+'&em='+str(em)+'&code='+str(code)+'&apply=0&df='+str(df)+'&mf='+str(mf)+'&yf='+str(yf)+'&from='+str(day_start)+'.'+str(month_start)+'.'+str(yf)+'&dt='+str(dt)+'&mt='+str(mt)+'&yt='+str(yt)+'&to='+str(day_end)+'.'+str(month_end)+'.'+str(yt)+'&p='+str(p)+'&f='+str(code)+'_'+str(year_start)+str(month_start)+str(day_start)+'_'+str(year_end)+str(month_end)+str(day_end)+'&e='+str(e)+'&cn='+str(code)+'&dtf='+str(dtf)+'&tmf='+str(tmf)+'&MSOR='+str(MSOR)+'&mstimever='+str(mstimever)+'&sep='+str(sep)+'&sep2='+str(sep2)+'&datf='+str(datf)+'&at='+str(at))
-    f = open("company_quotes.txt", "w")
+    f = open("history/" + code + ".txt", "w")
     content = page.read()
     f.write(content)
     f.close()
 
 def getLastYearQuotes(name, quoteId):
-    code='NOK';
+    code=name;
     e='.txt';
     market='1'
-    em='874609';
+    em=quoteId;
     e='.txt';
     p='8';
     yf='2019';
@@ -47,3 +46,11 @@ def getLastYearQuotes(name, quoteId):
     dt=(int(day_end.replace('0','')))-1;
 
     qq = quotes(code,year_start,month_start,day_start,year_end,month_end,day_end,e,market,em,df,mf,yf,dt,mt,yt,p,dtf,tmf,MSOR,mstimever,sep,sep2,datf,at)
+
+
+
+name = "NOK"
+quoteId = '874609'
+print("Grabbing " + name + " history...")
+getLastYearQuotes(name, quoteId)
+print("Done")
